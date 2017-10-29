2cddb1e161a31651ad5f57e5c2d787157856ac94
fullstacktechnos <accounts@fullstacktechnos.com>
2017-10-29 20:07:46 +0530

    dummy config

---
+++ public/firebaseconf.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "dummayapaikey",
+    authDomain: "testdemo.firebaseapp.com",
+    databaseURL: "https://testdemo.firebaseio.com",
+    projectId: "testdemo",
+    storageBucket: "testdemo.appspot.com",
+    messagingSenderId: "111111111111"
+};
+firebase.initializeApp(config);

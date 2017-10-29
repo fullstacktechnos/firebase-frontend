d244e668c1f45859657ea06a4483fe7e97a311cf
rajapattanayak <pattanayak.raja@gmail.com>
2017-10-29 20:06:02 +0530

    Minor change

--- public/firebaseconf.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "dummayapaikey",
-    authDomain: "testdemo.firebaseapp.com",
-    databaseURL: "https://testdemo.firebaseio.com",
-    projectId: "testdemo",
-    storageBucket: "testdemo.appspot.com",
-    messagingSenderId: "111111111111"
-};
-firebase.initializeApp(config);
